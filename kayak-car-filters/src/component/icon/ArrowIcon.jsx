ae0646f343ad42dc5ce665a2fb77598d3fbf1ff8
Deividas Popelskis <deividas.popelskis@gmail.com>
2018-08-14 22:10:24 +0300

    Adjust icon styles

--- kayak-car-filters/src/component/icon/ArrowIcon.jsx
+++ kayak-car-filters/src/component/icon/ArrowIcon.jsx
@@ -4,9 +4,11 @@ function ArrowIcon(props) {
     const { reverse } = props;
 
     return (
-        <svg className={(reverse ? 'icon reverse' : 'icon')} viewBox="0 0 8 5" stroke-linejoin="round" stroke-linecap="round" stroke-width="1.35">
-            <path d="M7 1.053L4.027 4 1 1" stroke="currentColor" fill="none" />
-        </svg>
+        <div className="icon">
+            <svg className={(reverse ? 'reverse' : '')} viewBox="0 0 8 5" stroke-linejoin="round" stroke-linecap="round" stroke-width="1.35">
+                <path d="M7 1.053L4.027 4 1 1" stroke="currentColor" fill="none" />
+            </svg>
+        </div>
     );
 }
 
--- kayak-car-filters/src/component/icon/RemoveIcon.jsx
+++ kayak-car-filters/src/component/icon/RemoveIcon.jsx
@@ -4,9 +4,11 @@ function RemoveIcon(props) {
     const { onClick } = props;
 
     return (
-        <svg onClick={onClick} viewBox="0 0 9.85 9.93" className="icon remove-icon">
-            <path fill="none" stroke="currentColor" strokeMiterlimit="10" strokeWidth="2px" d="M.71.79l8.43 8.43M9.14.71L.71 9.14" />
-        </svg>
+        <div className="icon remove-icon">
+            <svg onClick={onClick} viewBox="0 0 9.85 9.93">
+                <path fill="none" stroke="#1e93f5" strokeMiterlimit="10" strokeWidth="2px" d="M.71.79l8.43 8.43M9.14.71L.71 9.14" />
+            </svg>
+        </div>
     );
 }
 
